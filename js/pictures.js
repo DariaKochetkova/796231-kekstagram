a4072cf518e2a3d8941249253fbff8141998a7a4
Daria <42115994+DariaKochetkova@users.noreply.github.com>
2018-11-29 18:37:34 +0500

    Отредактировала третье задание

--- js/pictures.js
+++ js/pictures.js
@@ -3,20 +3,23 @@
 var pictures = [];
 var descriptions = ['Тестим новую камеру!', 'Затусили с друзьями на море', 'Как же круто тут кормят', 'Отдыхаем...', 'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......', 'Вот это тачка!'];
 var comments = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-var commentsMax = 10;
-var commentsMin = 5;
-var likesMax = 200;
-var likesMin = 15;
-var avatarsMax = 6;
-var avatarsMin = 1;
+var OBJECTS_QUANTITY = 25;
+var COMMENTS_MAX = 10;
+var COMMENTS_MIN = 5;
+var LIKES_MAX = 200;
+var LIKES_MIN = 15;
+var AVATARS_MAX = 6;
+var AVATARS_MIN = 1;
+var DESCRIPTIONS_MAX = 6;
+var DESCRIPTIONS_MIN = 6;
 var getRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 var getRandomElement = function (arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
+  return arr[getRandomNumber(DESCRIPTIONS_MIN, DESCRIPTIONS_MAX)];
 };
 var getRandomComment = function () {
-  var commentsQuantity = getRandomNumber(commentsMin, commentsMax);
+  var commentsQuantity = getRandomNumber(COMMENTS_MIN, COMMENTS_MAX);
   var currentComments = [];
   for (var i = 0; i < commentsQuantity; i++) {
     var randomComment = getRandomElement(comments);
@@ -25,10 +28,10 @@ var getRandomComment = function () {
   return currentComments;
 };
 
-for (var i = 0; i < 25; i++) {
+for (var i = 0; i < OBJECTS_QUANTITY; i++) {
   pictures[i] = {
     url: 'photos/' + (i + 1) + '.jpg',
-    likes: getRandomNumber(likesMin, likesMax),
+    likes: getRandomNumber(LIKES_MIN, LIKES_MAX),
     comments: getRandomComment(),
     description: getRandomElement(descriptions)
   };
@@ -49,7 +52,7 @@ var renderPicture = function (picture) {
 };
 
 var fragment = document.createDocumentFragment();
-for (var j = 0; j < 25; j++) {
+for (var j = 0; j < OBJECTS_QUANTITY; j++) {
   fragment.appendChild(renderPicture(pictures[j]));
 }
 picturesContainer.appendChild(fragment);
@@ -67,11 +70,11 @@ commentsCount.textContent = pictures[1].comments.length;
 var commentsFragment = document.createDocumentFragment();
 var commentsList = document.querySelector('.social__comments');
 
-for (var y = 0; y < pictures[1].comments.length; y++) {
+for (var k = 0; k < pictures[1].comments.length; k++) {
   var commentElement = document.createElement('li');
   commentElement.className = 'social__comment';
-  commentElement.innerHTML = '<img class="social__picture" src="img/avatar-' + getRandomNumber(avatarsMin, avatarsMax) + '.svg" alt="Аватар комментатора фотографии" width="35" height="35"></img>';
-  commentElement.innerHTML += '<p class="social__text">' + pictures[1].comments[y] + '</p>';
+  commentElement.innerHTML = '<img class="social__picture" src="img/avatar-' + getRandomNumber(AVATARS_MIN, AVATARS_MAX) + '.svg" alt="Аватар комментатора фотографии" width="35" height="35"></img>';
+  commentElement.innerHTML += '<p class="social__text">' + pictures[1].comments[k] + '</p>';
   commentsFragment.appendChild(commentElement);
 }
 
