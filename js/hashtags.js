6c15d06a632f9e4d0dc88338e3d650a65e4abed6
balesniy <balesniy@yandex.ru>
2019-01-12 17:23:40 +0100

    разрешает пробелы в тегах

--- js/hashtags.js
+++ js/hashtags.js
@@ -7,60 +7,60 @@
   var HASH_SYMBOL = '#';
 
   var setErrorColor = function () {
-    window.hashtags.field.style = 'border: 3px solid red;';
+    window.hashtags.field.style.border = '3px solid red';
   };
   var deleteErrorColor = function () {
-    window.hashtags.field.style = '';
+    window.hashtags.field.style.border = 'none';
+  };
+  var setErrorMessage = function (error) {
+    setErrorColor();
+    window.hashtags.field.setCustomValidity(error);
   };
   window.hashtags = {
     field: document.querySelector('.text__hashtags'),
     onFieldChange: function () {
       window.hashtags.field.setCustomValidity('');
-      var hashtags = window.hashtags.field.value.split(' ');
-      window.hashtags.field.value.toLowerCase();
-      var uniquehashtags = {};
+      deleteErrorColor();
+      var hashtags = window.hashtags.field.value.trim().toLowerCase().split(/\s+/);
+      var uniqueHashtags = {};
       var checkHashtagsQuantity = function () {
         return hashtags.length > MAX_HASHTAGS ? 'Количество хэш-тегов не может превышать пять' : '';
       };
-      var tagsQuantityError = checkHashtagsQuantity();
-      var setErrorMessage = function (error) {
-        setErrorColor();
-        window.hashtags.field.setCustomValidity(error);
+      var checkHash = function (hashtag) {
+        return hashtag.charAt(0) === HASH_SYMBOL ? '' : 'Хэш-тег должен начинаться с "#"';
       };
+      var checkSpaces = function (hashtag) {
+        var hashtagIndex = hashtag.lastIndexOf(HASH_SYMBOL);
+        return hashtagIndex !== 0 ? 'Хэш-теги должны разделяться пробелами' : '';
+      };
+      var checkMinLength = function (hashtag) {
+        return hashtag.length < MIN_HASHTAG_LENGTH ? 'Хэш-тег не может состоять из одной решетки' : '';
+      };
+      var checkMaxLength = function (hashtag) {
+        return hashtag.length > MAX_HASHTAG_LENGTH ? 'Длина хэш-тега не может быть более двадцати символов' : '';
+      };
+      var checkRepeat = function (hashtag) {
+        return uniqueHashtags[hashtag] ? 'Хэш-теги не могут повторяться' : '';
+      };
+      var checkHashtag = function (hashtag) {
+        return checkHash(hashtag) ||
+          checkSpaces(hashtag) ||
+          checkMinLength(hashtag) ||
+          checkMaxLength(hashtag) ||
+          checkRepeat(hashtag);
+      };
+      var tagsQuantityError = checkHashtagsQuantity();
+
       if (tagsQuantityError) {
         setErrorMessage(tagsQuantityError);
       } else {
         for (var n = 0; n < hashtags.length; n++) {
-          deleteErrorColor();
-          if (window.hashtags.field.value === '') {
+          var hashtagKey = hashtags[n];
+          if (hashtagKey === '') {
             break;
           }
-          var hashtagKey = hashtags[n];
-          var checkHash = function (hashtag) {
-            return hashtag.charAt(0) === HASH_SYMBOL ? '' : 'Хэш-тег должен начинаться с "#"';
-          };
-          var checkSpaces = function () {
-            var hashtagIndex = hashtags[n].lastIndexOf(HASH_SYMBOL);
-            return hashtagIndex !== 0 ? 'Хэш-теги должны разделяться пробелами' : '';
-          };
-          var checkMinLength = function (hashtag) {
-            return hashtag.length < MIN_HASHTAG_LENGTH ? 'Хэш-тег не может состоять из одной решетки' : '';
-          };
-          var checkMaxLength = function (hashtag) {
-            return hashtag.length > MAX_HASHTAG_LENGTH ? 'Длина хэш-тега не может быть более двадцати символов' : '';
-          };
-          var checkRepeat = function () {
-            return uniquehashtags[hashtagKey] ? 'Хэш-теги не могут повторяться' : '';
-          };
-          var checkHashtag = function (hashtag) {
-            return checkHash(hashtag) ||
-            checkSpaces() ||
-            checkMinLength(hashtag) ||
-            checkMaxLength(hashtag) ||
-            checkRepeat();
-          };
-          var tagError = checkHashtag(hashtags[n]);
-          uniquehashtags[hashtagKey] = true;
+          var tagError = checkHashtag(hashtagKey);
+          uniqueHashtags[hashtagKey] = true;
           if (tagError) {
             setErrorMessage(tagError);
             break;
